809f250cce87c5b38967cd9ec32e366fadddcbe4
jl4anger <scherer.joao@escola.pr.gov.br>
2025-06-16 11:28:15 +0000

    alterações

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Jogos',
+    'Estamos para o que esta semana?',
+    'Minecraft'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual o maior time do Sul do Brasil?',
+    'O Sport Club Internacional'
+)
